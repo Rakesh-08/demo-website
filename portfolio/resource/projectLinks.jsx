76db5ec8facc890eb7529c775eaae0d5ab0e3dce
Rakesh Mandal <rkm8448746347@gmail.com>
2023-06-01 13:15:41 +0530

    basic version remains only

--- portfolio/resource/projectLinks.jsx
+++
@@ -1,9 +0,0 @@
-let projects = [
-    { name: "Todo-App", link:"https://simple-todo-app-psi.vercel.app/" },
-    { name: "Counter-App", link:"#" },
-    { name: "Tic-Tac-Toe Game", link:"https://simple-todo-app-git-tic-tac-rakesh-08.vercel.app/" },
-    { name: "Resume Builder", link: "#" },
-    { name:" continue to be >> chat-bot",link:"#" }
-  
-];
-export default projects;
