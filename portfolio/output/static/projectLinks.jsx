a679aa6631b646d45dc5a85aaa75d803aed7e95d
Rakesh Mandal <rkm8448746347@gmail.com>
2023-06-03 11:48:43 +0530

    second final commit

--- portfolio/output/static/projectLinks.jsx
+++ portfolio/output/static/projectLinks.jsx
@@ -1,9 +1,14 @@
 let projects = [
-    { name: "Todo-App", link:"https://simple-todo-app-psi.vercel.app/" },
-    { name: "Counter-App", link:"#" },
-    { name: "Tic-Tac-Toe Game", link:"https://simple-todo-app-git-tic-tac-rakesh-08.vercel.app/" },
-    { name: "Resume Builder", link: "#" },
-    { name:" coming soon.. >> chat-bot",link:"#" }
-  
+  { name: "Todo-App", link: "https://simple-todo-app-psi.vercel.app/" },
+  { name: "Counter-App", link: "#" },
+  {
+    name: "Tic-Tac-Toe Game",
+    link: "https://simple-todo-app-git-tic-tac-rakesh-08.vercel.app/",
+  },
+  {
+    name: "Resume Builder",
+    link: "https://github.com/Rakesh-08/resume-builder",
+  },
+  { name: " coming soon.. >> chat-bot", link: "#" },
 ];
 export default projects;
