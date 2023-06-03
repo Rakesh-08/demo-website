590286d3a5fbb6bf8b9291bf8cada499ea293e9c
Rakesh Mandal <87007801+Rakesh-08@users.noreply.github.com>
2023-06-03 12:20:58 +0530

    Update projectLinks.jsx

--- portfolio/output/static/projectLinks.jsx
+++ portfolio/output/static/projectLinks.jsx
@@ -1,6 +1,6 @@
 let projects = [
   { name: "Todo-App", link: "https://simple-todo-app-psi.vercel.app/" },
-  { name: "Counter-App", link: "#" },
+  { name: "Counter-App", link: "https://rakesh-08.github.io/Rakesh-08/Bits-and-Pieces/Dom/counter%20App/counter.html" },
   {
     name: "Tic-Tac-Toe Game",
     link: "https://simple-todo-app-git-tic-tac-rakesh-08.vercel.app/",
