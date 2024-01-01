c3f28dbdfbcfd0590c8d21772360cf283d55fa0e
Rakesh Mandal <87007801+Rakesh-08@users.noreply.github.com>
2024-01-01 14:05:25 +0530

    Update Home.jsx

--- portfolio/src/components/Home/Home.jsx
+++ portfolio/src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import "./Home.css";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
-import YoutubeIcon from "@mui/icons-material/Youtube";
+import YouTubeIcon from '@mui/icons-material/YouTube';
 import GitHubIcon from "@mui/icons-material/GitHub";
 import NavigationBtn from "../NavigationBtn/NavigationBtn";
 
