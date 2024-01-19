10ab43bab30eabc99fa2b57e4911e067f10d97fe
Rakesh Mandal <rkm8448746347@gmail.com>
2024-01-19 16:35:50 +0530

    UI mofications

--- portfolio/src/components/Home/Home.jsx
+++ portfolio/src/components/Home/Home.jsx
@@ -11,10 +11,13 @@ export default function Home() {
   return (
     <div className="home" id="home">
       <div className="welcomeMsg">
-        <h3 className="display-4 ">
-          <span className="firstN">Rakesh</span>
-          <span className="lastN"> Mandal</span>
-        </h3>
+        <marquee direction="right" behavior="alternate" scrollDelay={65}>
+          {" "}
+          <h3 className="display-4 ">
+            <span className="firstN">Rakesh</span>
+            <span className="lastN"> Mandal</span>
+          </h3>
+        </marquee>
       </div>
       <div className="homeContainer mx-2">
         <div className="homeRight my-3 ">
@@ -27,63 +30,69 @@ export default function Home() {
 
         <div className="homeLeft mx-2">
           <div>
-          <p>
-            <span style={{ color: "orange", lineHeight: "3em" }}>
-              Hello everyone,
-            </span>
-            <br />I am a Web developer with a great interest for new
-            Technologies. I have experise in javascript, nodejs ,
-            expressjs,mongoDB , redux and reactjs . I have created and deployed several full
-            stack projects and now I want to get started
-            in this industry and sharpen my skills by collaborating with industry experts in the creation of real-life technical products.
-          </p>
+            <p>
+              <span style={{ color: "orange", lineHeight: "3em" }}>
+                Hello everyone,
+              </span>
+              <br />I am a Web developer with a great interest for new
+              Technologies. I have experise in javascript, nodejs ,
+              expressjs,mongoDB , redux and reactjs . I have created and
+              deployed several full stack projects and now I want to get started
+              in this industry and sharpen my skills by collaborating with
+              industry experts in the creation of real-life technical products.
+            </p>
 
-          <div className='my-5' >
-            <span style={{background:"linear-gradient(90deg,black 40%,green)"}} className="fst-italic fs-5 p-2 rounded  ">connect with me @ </span>
-            <div className="socialChannels my-4">
-              <div className="media">
-                <a
-                  href="https://www.instagram.com/rakesh_mandal080499/"
-                  target="_blank"
-                >
-                  <InstagramIcon className="icon" />
-                </a>
-              </div>
+            <div className="my-5">
+              <span
+                style={{ background: "linear-gradient(90deg,black 40%,green)" }}
+                className="fst-italic fs-5 p-2 rounded  "
+              >
+                connect with me @{" "}
+              </span>
+              <div className="socialChannels my-4">
+                <div className="media">
+                  <a
+                    href="https://www.instagram.com/rakesh_mandal080499/"
+                    target="_blank"
+                  >
+                    <InstagramIcon className="icon" />
+                  </a>
+                </div>
 
-              <div className="media">
-                <a
-                  href="https://www.facebook.com/profile.php?id=100009636102737"
-                  target="_blank"
-                >
-                  <FacebookIcon className="icon" />
-                </a>
-              </div>
+                <div className="media">
+                  <a
+                    href="https://www.facebook.com/profile.php?id=100009636102737"
+                    target="_blank"
+                  >
+                    <FacebookIcon className="icon" />
+                  </a>
+                </div>
 
-              <div className="media">
-                <a
-                  href="https://www.linkedin.com/in/rakesh-mandal-27b809193"
-                  target="_blank"
-                >
-                  <LinkedInIcon className="icon" />
-                </a>
-              </div>
+                <div className="media">
+                  <a
+                    href="https://www.linkedin.com/in/rakesh-mandal-27b809193"
+                    target="_blank"
+                  >
+                    <LinkedInIcon className="icon" />
+                  </a>
+                </div>
 
-              <div className="media">
-                <a href="https://github.com/Rakesh-08" target="_blank">
-                  <GitHubIcon className="icon" />
-                </a>
-              </div>
-              <div className="media">
-                <a
-                  href="https://www.youtube.com/channel/UCFQz6AUsISAJOCMwWYxVSGA"
-                  target="_blank"
-                >
-                  <YouTubeIcon className="icon" />
-                </a>
+                <div className="media">
+                  <a href="https://github.com/Rakesh-08" target="_blank">
+                    <GitHubIcon className="icon" />
+                  </a>
+                </div>
+                <div className="media">
+                  <a
+                    href="https://www.youtube.com/channel/UCFQz6AUsISAJOCMwWYxVSGA"
+                    target="_blank"
+                  >
+                    <YouTubeIcon className="icon" />
+                  </a>
+                </div>
               </div>
             </div>
-            </div>
-            </div>
+          </div>
         </div>
       </div>
     </div>
--- portfolio/src/components/Projects/Projects.jsx
+++ portfolio/src/components/Projects/Projects.jsx
@@ -10,12 +10,12 @@ export default function Projects() {
         <div className="projects" id="projects">
             <h2 className="my-5 fs-4">{"< My Projects />"}</h2>
         <div className="projectsHeading">
-          <p className="text-secondary">
+          <p style={{color:"rgb(222,288,233)"}}>
              I have created these applications with the help of MERN stack technologies such as frontend is created using javascript ,reactjs,redux and bootstrap while backend is being built upon nodejs,expressjs and MongoDB.
           </p>
             
             </div>
-        <div className="projectContainer row">
+        <div className="projectContainer  row">
           <div className="d-flex flex-wrap col-md-12 ">
              {projects.map((proj, index) => {
             return <Project key={index} name={proj.name} link={proj.link} poster={proj.poster} />;
